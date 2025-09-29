b1d40eb32bea38b7d7446cb0ed8806426f17c6db
kanavsachdeva41 <kanavsachdeva41@gmail.com>
2025-09-29 12:54:09 +0530

    assignment_1

---
+++ assi1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void main()
+{
+    char vehnum[10];
+
+    printf("Enter Vehicle Number: ");
+    scanf("%s", &vehnum);
+
+    float enh, enm, exh, exm;
+
+    printf("Enter Entry Time (HH MM): ");
+    scanf("%f %f", &enh, &enm);
+
+    printf("Enter Exit Time (HH MM): ");
+    scanf("%f %f", &exh, &exm);
+
+    float enh1 = enm / 60, exh1 = exm / 60;
+    int enth = enh + enh1, exth = exh + exh1;
+
+    int tdur = exth - enth, amount;
+
+    if (tdur <= 2)
+    {
+        amount = tdur * 2;
+    }
+    else
+    {
+        amount = 10 + (tdur - 2) * 3;
+    }
+
+    printf("\n---- Parking Bill ----\n");
+    printf("Vehicle Number: %s\n", vehnum);
+    printf("Total duration: %d hours\n", tdur);
+    printf("Total amount: $%d\n", amount);
+}
